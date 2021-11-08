016c4e53634325b83608cd8269aa4c529b94dc69
Jimmy Nguyen <gymmewin@Jimmys-MacBook-Pro.local>
2021-11-08 10:36:16 -0500

    users created

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 
 //Controllers
 const carsController = require('./controllers/cars.js')
+const userController = require('./controllers/users_controller.js')
 app.use('/cars', carsController)
+app.use('/', userController)
 
 app.get('/', (req,res) => {
    res.send('Hello World')
